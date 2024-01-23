acf2bec378b306d63068102f0711ebfb5a3bf778
cethread <threadgillcheyenne@gmail.com>
2024-01-23 17:11:25 -0500

    r

--- server/server.js
+++ server/server.js
@@ -4,12 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(
-  cors({
-    origin: "https://kubermanagement.netlify.app/",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8080;
