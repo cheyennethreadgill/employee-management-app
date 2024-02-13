2b0cc3d41f154571653602f7f18407cb1ad78033
cethread <threadgillcheyenne@gmail.com>
2024-02-13 15:47:16 -0500

    r

--- client/src/Components/Employees/AllEmployees.jsx
+++ client/src/Components/Employees/AllEmployees.jsx
@@ -374,7 +374,7 @@ const AllEmployees = ({ URL, handleFetchPromiseError, handleJsonPromiseResponseL
           </section>
         </section>
       </Container>
-      )
+
       {!showNow ? null : (
         <MyModal
           employees={employees}
