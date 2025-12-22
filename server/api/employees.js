b463448334f296549f8862aa0b5a37e53620c053
cethread <threadgillcheyenne@gmail.com>
2025-12-22 18:42:00 -0500

    server,js change

--- server/api/employees.js
+++ server/api/employees.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import aws from "aws-sdk";
 import { connectDB } from "./database.js";
 
-const employeeRouter = new Router();
+export const employeeRouter = new Router();
 
 // // Create S3 instance
 // const s3 = new aws.S3();
@@ -335,5 +335,3 @@ employeeRouter.delete("/delete-employee/:email", async (req, res) => {
     res.json({ message: err });
   }
 });
-
-export default employeeRouter;
