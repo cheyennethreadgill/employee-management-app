845fd0849f196dae23c9d4ab3ddbb6e98ec7ed70
cethread <threadgillcheyenne@gmail.com>
2025-12-22 17:57:09 -0500

    index.js change

--- src/index.jsx
+++ src/index.jsx
@@ -23,7 +23,7 @@ export const CustomContext = createContext(null);
 const Index = () => {
   // URL
   // const URL = "http://localhost:8080/"; // Server data: use when in local development
-  // const URL = "https://employee-management-app-rho.vercel.app/"; // use when in production
+  const URL = "https://employee-management-app-rho.vercel.app/"; // use when in production
 
   const EMPLOYEES_API_PATH = "api/employees";
   const ADDEMPLOYEE_PATH = "admin/add-employee";
