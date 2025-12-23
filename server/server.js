7b7da840dc02d1bc4fa9777339b343e44981ad3a
cethread <threadgillcheyenne@gmail.com>
2025-12-22 20:52:21 -0500

    employees.js change

--- server/server.js
+++ server/server.js
@@ -48,35 +48,35 @@ app.use("/", (req, res) => {
 });
 
 // app.use("/api", employeeRouter);
-// app.use("/api", async (req, res, next) => {
-//   try {
-//     // awaiting a new database connection
-//     // const db = await connectDB();
+app.use("/api", async (req, res, next) => {
+  try {
+    // awaiting a new database connection
+    // const db = await connectDB();
 
-//     // const employees = await db.collection("employees").find({}).toArray();
+    // const employees = await db.collection("employees").find({}).toArray();
 
-//     // console.log(employees, "<<<<<<<<<employees in api");
-//     // return res.status(200).json(employees);
+    // console.log(employees, "<<<<<<<<<employees in api");
+    // return res.status(200).json(employees);
 
-//     // *************************testing
-//     res.send("employees api json working (server.js)");
-//     // res.send(employees);
-//   } catch (err) {
-//     return res.status(500).json({ error: err.message });
-//   }
-//   // try {
-//   //   let foundArray = [];
-//   //   const allEmployeesFound = employees.find();
+    // *************************testing
+    res.send("employees api json working (server.js)");
+    // res.send(employees);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+  // try {
+  //   let foundArray = [];
+  //   const allEmployeesFound = employees.find();
 
-//   //   for await (const doc of allEmployeesFound) {
-//   //     foundArray.push(doc);
-//   //   }
-//   //   res.json(foundArray);
-//   // } catch (err) {
-//   //   console.log(`************error getting employees in employee route: ${err}`);
-//   //   next(err);
-//   // }
-// });
+  //   for await (const doc of allEmployeesFound) {
+  //     foundArray.push(doc);
+  //   }
+  //   res.json(foundArray);
+  // } catch (err) {
+  //   console.log(`************error getting employees in employee route: ${err}`);
+  //   next(err);
+  // }
+});
 
 // ***************************** TESTING
 // app.use("/api", (req, res) => {
