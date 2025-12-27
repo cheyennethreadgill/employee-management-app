60d9e24d253e2bd7a233aae457260ac50a69c543
cethread <threadgillcheyenne@gmail.com>
2025-12-27 17:31:27 -0500

    created api folder with exported app, modified employee route handlers

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,4 @@
-import app from "../../server/server";
+import app from "../../server/server.js";
 
 // Wrap the Express app with serverless-http so Vercel can invoke it as a handler
 export default app;
