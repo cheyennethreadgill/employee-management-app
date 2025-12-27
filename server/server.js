fbbd41e70cc6de8deaea5cb1aa9d7e5ec27a5662
cethread <threadgillcheyenne@gmail.com>
2025-12-27 15:40:33 -0500

    created api folder with exported app, modified employee route handlers

---
+++ server/server.js
@@ -0,0 +1,68 @@
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import authRouter from "./Routes/auth.routes.js";
+import { getProjectsRouter, addProjectsRouter, updateProjectsRouter } from "./Routes/project.routes.js";
+import { employeeRouter } from "./employees.js";
+import { connectDB } from "./database.js";
+import multer from "multer";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// upload to multer memory storage
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+// Configure AWS SDK with environment variables
+
+const corsOptions = {
+  origin: "*",
+  methods: "*",
+  allowedHeaders: "*",
+};
+
+// waiting for the DB to connect
+await connectDB();
+
+// middleware used for entire application
+app.use(cors(corsOptions));
+
+//requests handled already at app level
+
+app.use(
+  bodyParser.json({
+    limit: 10000000,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true, limit: 10000000 }));
+
+app.use(upload.single("image"));
+
+// all requests to sign up and login with be router using router in auth.routes
+// app.use("/auth", authRouter);
+
+app.use("/api", employeeRouter);
+
+app.use("/", (req, res) => {
+  res.send("Welcome to the Kuber Employee Management API! (Root)");
+});
+// app.use("/api", (req, res, next) => {
+//   res.send("employees api json working (server.js)");
+// });
+
+// ***************************** TESTING
+// app.use("/api", (req, res) => {
+//   res.send("employees api json working (employee.js)");
+// });
+
+// app.use("/add-project", addProjectsRouter);
+// app.use("/all-projects", getProjectsRouter);
+// app.use("/update-project", updateProjectsRouter);
+
+app.listen(PORT, () => {
+  console.log(`***********Server running on port ${PORT}...*********`);
+});
+
+export default app;
---
+++ server/utils/updateOrCreateUserFromOAuth.js
@@ -0,0 +1,54 @@
+import { connectDB } from "../database.js";
+// take user info from google oath and create new user in db
+
+// user from get user
+export const updateOrCreateUserFromOAuth = async (req, res, next, { oauthUserInfo }) => {
+  const { id: googleID, verified_email, email, given_name, family_name } = oauthUserInfo;
+
+  const db = await connectDB();
+
+  try {
+    // check to see if user is already in db
+
+    const userExists = await db(process.env.MONGODB_DBNAME)
+      .collection("employees")
+      .findOne({
+        email,
+      })
+      .toArray();
+
+    if (userExists) {
+      console.log("****userExists");
+      await employees.findOneAndUpdate(
+        { email },
+        {
+          $set: {
+            googleID,
+            isVerified: verified_email,
+          },
+        }
+      );
+      const foundInsertedUser = await employees.findOne({
+        email,
+      });
+      return foundInsertedUser;
+    } else {
+      // // *****saving user with mongoose
+      await employees.insertOne({
+        googleID,
+        fname: given_name,
+        lname: family_name,
+        email,
+        isVerified: verified_email,
+      });
+      const foundInsertedUser = await employees.findOne({
+        email,
+      });
+
+      return foundInsertedUser;
+    }
+  } catch (err) {
+    next(err);
+    // return res.status(500).json({ message: err });
+  }
+};
