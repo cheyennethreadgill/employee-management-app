0a28963188e8a853b57280558f2ea98ea879647d
cethread <threadgillcheyenne@gmail.com>
2025-12-27 19:10:19 -0500

    created api folder with exported app, modified employee route handlers

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,6 @@
-import app from "../server";
+import app from "../server.js";
 import ServerlessHttp from "serverless-http";
+
+console.log("api/index.js imported");
 
 export default ServerlessHttp(app);
