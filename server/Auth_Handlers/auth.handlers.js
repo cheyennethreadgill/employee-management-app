73c700f745eb7949b2a8083d3fcac450315dfc18
cethread <threadgillcheyenne@gmail.com>
2025-12-30 17:39:36 -0500

    changed error log

--- server/Auth_Handlers/auth.handlers.js
+++ server/Auth_Handlers/auth.handlers.js
@@ -78,8 +78,8 @@ export const loginHandler = async (req, res) => {
       const { _id, fname, lname, username, password, email, image } = foundUser;
       // compare password against db, result is a boolean
       bcrypt.compare(passwordBody, password, (err, result) => {
-        console.log("************************result from bcrypt compare", err);
         // if bycrypt fn errors, log error
+        res.send("************************result from bcrypt compare", result);
         if (err) {
           res.sendStatus(409).json({ message: "*****Login encryption error." });
         }
