ecf8e516851c2f19e5aa7f71d7ad15a8d7e13eda
cethread <threadgillcheyenne@gmail.com>
2026-02-25 17:04:07 -0500

    added admin priveldges (hardcoded admin info)

--- server/Routes/employees.js
+++ server/Routes/employees.js
@@ -243,7 +243,8 @@ employeeRouter.put("/update-employee/:id", async (req, res, next) => {
     // if id sent from client doesnt equal the id that they want to update
     if (id !== decoded.id) {
       return res.status(409).json({ message: "Access denied. You need admin privledges to make this change." });
-    } else if (id === "664100fd3f2c3c90eb122546") {
+    }
+    if (id === "664100fd3f2c3c90eb122546") {
       console.log("access granted");
       return;
     }
